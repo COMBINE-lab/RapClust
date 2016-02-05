d993c96e83778acac3bcce49fb7589d9edcbd5d6
Rob Patro <rob.patro@gmail.com>
2016-02-05 16:52:04 -0500

    Prepare rapclust as a pypi package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='rapclust',
+      version='0.1',
+      scripts=['bin/RapClust'],
+      description='Accurate, Fast and Lightweight Clustering of de novo Transcriptomes using Fragment Equivalence Classes',
+      url='https://github.com/COMBINE-lab/RapClust',
+      author='Avi Srivastava, Hirak Sarkar, Laraib Malik, Rob Patro',
+      author_email='rob.patro@cs.stonybrook.edu',
+      license='BSD with attribution',
+      packages=['rapclust'],
+      install_requires=[
+          'PyYAML',
+          'click',
+          'numpy',
+          'pandas'
+      ],
+      zip_safe=False)
